ec92626d923fd28dc05702636bf6f3a964255b65
thanos <koutroubas.thanos@gmail.com>
2021-04-06 04:37:47 +0300

    Added second axis (uptime)

--- create_plot.py
+++ create_plot.py
@@ -1,7 +1,29 @@
 import argparse
 import csv
+import datetime
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+def get_secs(time_str):
+    """Get Seconds from time."""
+    h = time_str.split('h')[0]
+    m = time_str.split('h')[1].split('min')[0]
+    s = time_str.split('h')[1].split('min')[1].split('s')[0]
+
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
+def get_hours(time_str):
+    """Get Seconds from time."""
+    h = time_str.split('h')[0]
+    m = time_str.split('h')[1].split('min')[0]
+    s = time_str.split('h')[1].split('min')[1].split('s')[0]
+
+    return round(int(h) + int(m) / 60 + int(s) / 3600, 2)
+
+def conv_sec_to_timedelta(seconds):
+    return str(datetime.timedelta(seconds=seconds))
+
 
 if __name__ == "__main__":
 
@@ -47,6 +69,11 @@ if __name__ == "__main__":
                     if key == 'current_date':
                         ts = pd.to_datetime(tokens[i].strip(), format='%Y-%m-%d %H:%M:%S', errors='ignore')
                         data[key].append(ts)
+                    elif key == 'showtime_start_value':
+                        secs = get_secs(tokens[i].strip())
+                        hours = get_hours(tokens[i].strip())
+                        # data[key].append(secs)
+                        data[key].append(hours)
                     else:
                         # Try to convert the int values
                         try:
@@ -67,34 +94,43 @@ if __name__ == "__main__":
     df_data = df_data.set_index('current_date')
 
     # Delete non-error like columns (TEMP)
-    try:
-        df_data = df_data.drop(['showtime_start_value'], axis=1)
-    except KeyError:
-        pass
-    print(df_data)
+    # try:
+    #     df_data = df_data.drop(['showtime_start_value'], axis=1)
+    # except KeyError:
+    #     pass
+    # print(df_data)
+
 
     # Create plot
+    # fig = go.Figure([go.Scatter(y=df_data[key], x=df_data.index, name=df_data[key].name) for key in df_data])
 
-    # fig = px.line(df_data)
-    fig = go.Figure([go.Scatter(y=df_data[key], x=df_data.index, name=df_data[key].name) for key in df_data])
+    # Create figure with secondary y-axis
+    fig = make_subplots(specs=[[{'secondary_y': True}]])
 
-    fig.update_layout(
-        yaxis_title='Errors',
-        xaxis_title=df_data.index.name,
-        # title='Continuous, variable value error bars',
-    )
+    # Add lines to figure
+    for key in df_data:
+        if key == 'showtime_start_value':
+            # For Showtime values, add second axis
+            fig.add_trace(go.Scatter(y=df_data[key], x=df_data.index, name=df_data[key].name), secondary_y=True)
+            fig.update_yaxes(title_text='<b>Uptime</b> (Hours)', secondary_y=True)
+        else:
+            fig.add_trace(go.Scatter(y=df_data[key], x=df_data.index, name=df_data[key].name), secondary_y=False)
 
+    # Add Titles
+    fig.update_layout(title='DSL Link Info Timeseries')
+    fig.update_xaxes(title_text='Date')
+    fig.update_yaxes(title_text='<b>Errors</b>', secondary_y=False)
 
     # Create Range Selector Slider and Buttons
     fig.update_xaxes(
         rangeslider_visible=True,
         rangeselector=dict(
             buttons=list([
-                dict(count=1, label="1d", step="day", stepmode="backward"),
-                dict(count=2, label="2d", step="day", stepmode="backward"),
-                dict(count=5, label="5d", step="day", stepmode="backward"),
-                dict(count=1, label="1m", step="month", stepmode="backward"),
-                dict(step="all")
+                dict(count=1, label='1d', step='day', stepmode='backward'),
+                dict(count=2, label='2d', step='day', stepmode='backward'),
+                dict(count=5, label='5d', step='day', stepmode='backward'),
+                dict(count=1, label='1m', step='month', stepmode='backward'),
+                dict(step='all')
             ])
         )
     )
